3d180c972dd4496b69943de3bb8b0e9ffa6b3272
Mosiah Valdez <val20004@byui.edu>
2024-02-21 17:56:09 -0700

    lets see

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
-app.get('/ryan', (req,res) => {res.send("Hello Ryan")}); 
+const express =require('express');
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req,res) => {res.send("Hello Browser")});
+
+app.get('/ryan', (req,res) => {res.send("Hello ryan")}); 
+app.get('/ian', (req,res) => {res.send("Hello Ian")}); 
+app.get('/Mosiah', (req,res) => {res.send("Hello Mosiah")});
+
+
+app.listen(port, ()=>console.log("Listening"));
+
 app.get('/ian', (req,res) => {res.send("Hello Ian")}); 
 app.get('/Mosiah', (req,res) => {res.send("Hello Mosiah")});
