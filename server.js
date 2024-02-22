2723cb989928caec45d5fe6c574fb45fd28d3f96
Mosiah Valdez <val20004@byui.edu>
2024-02-21 17:57:02 -0700

    try 2

--- server.js
+++ server.js
@@ -10,8 +10,4 @@ app.get('/ryan', (req,res) => {res.send("Hello ryan")});
 app.get('/ian', (req,res) => {res.send("Hello Ian")}); 
 app.get('/Mosiah', (req,res) => {res.send("Hello Mosiah")});
 
-
 app.listen(port, ()=>console.log("Listening"));
-
-app.get('/ian', (req,res) => {res.send("Hello Ian")}); 
-app.get('/Mosiah', (req,res) => {res.send("Hello Mosiah")});
