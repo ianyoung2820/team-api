282398bfae5a2b914f52741a52039b0d4935e0d7
Yorkd1 <davidgyork@gmail.com>
2024-02-22 16:56:10 -0700

    Added line for David

--- server.js
+++ server.js
@@ -9,5 +9,6 @@ app.get('/', (req,res) => {res.send("Hello Browser")});
 app.get('/ryan', (req,res) => {res.send("Hello ryan")}); 
 app.get('/ian', (req,res) => {res.send("Hello Ian")}); 
 app.get('/Mosiah', (req,res) => {res.send("Hello Mosiah")});
+app.get('/david', (req,res) => {res.send("Hello David")});
 
 app.listen(port, ()=>console.log("Listening"));
