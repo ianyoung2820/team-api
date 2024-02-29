a9f1f7bcca33e985bc13cf6c77bbb4b4a8b96233
Mosiah Valdez <val20004@byui.edu>
2024-02-28 17:13:33 -0700

    added to js file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req,res) => {res.send("Hello Browser")});
 
 app.get('/ryan', (req,res) => {res.send("Hello ryan")}); 
 app.get('/ian', (req,res) => {res.send("Hello Ian")}); 
-app.get('/Mosiah', (req,res) => {res.send("Hello Mosiah")});
+app.get('/mosiah', (req,res) => {res.send("Hello Mosiah")});
 app.get('/david', (req,res) => {res.send("Hello David")});
 
 app.listen(port, ()=>console.log("Listening"));
