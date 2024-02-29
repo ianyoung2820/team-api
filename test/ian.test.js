dc86cc18a40f3c48ccabdd1ded76cc3167971d76
ianyoung85 <104699600+ianyoung85@users.noreply.github.com>
2024-02-28 20:06:59 -0500

    Amended spelling error

--- test/ian.test.js
+++ test/ian.test.js
@@ -1,26 +1,7 @@
-const fetch = require("node-fetch2");
+const fetch = require('node-fetch2');
 
-it("Should get a login token",async ()=>{
-
-    let token = "";//this is an emptry string
-    const options = {
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body:JSON.stringify({
-            "userName":"scmurdock@gmail.com",
-            "password":"P@ssw0rd"
-        })
-    }
-
-    const response = await fetch('https://dev.stedi.me/login',options);
-
-    token = await response.text();
-    console.log("Token "+token);
-    const status = response.status;
-
-    expect(status).toBe(200);
-    expect(token.length).toBe(36);
-
-})
+test('Test for /ian endpoint', async () => {
+    const response = await fetch('http://localhost:3000/ian');
+    const name = await response.text();
+    expect(name).toBe('Hello Ian');
+});
